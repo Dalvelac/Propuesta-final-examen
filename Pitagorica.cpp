e5cb58408de50439a8b4bb5561a58cd75cc6a8dc
Daniel Alves Lacambra <dalvelac@myuax.com>
2024-10-29 10:09:46 +0000

    Programa

--- Pitagorica.cpp
+++ Pitagorica.cpp
@@ -4,8 +4,8 @@
 int main() {
     double a, b, c;
 
-    for (a = 1; a <= 20; a++) { // Aquí buscamos los valores de a, b y c que cumplan con la condición de ser un triángulo pitagórico
-        for (b = 1; b <= 20; b++) { 
+    for (a = 1; a <= 20; a++) { // Para a, se busca en el rango de 1 a 20
+        for (b = 1; b <= 20; b++) { // Para b, se busca en el rango de 1 a 20
             c = sqrt(pow(a, 2) + pow(b, 2)); // Fórmula del teorema de Pitágoras
             if (c == (c) && a + b > c && a + c > b && b + c > a) { // Condición para que sea un triángulo y c sea entero
                 if ((a == 3 && b == 4 && c == 5) || //Encontramos los valores de a, b y c que cumplen con la condiciónes: (3, 4, 5), (6, 8, 10), (5, 12, 13) y (8, 15, 17)
